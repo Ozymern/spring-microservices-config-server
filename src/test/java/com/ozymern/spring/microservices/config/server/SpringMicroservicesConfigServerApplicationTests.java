865fd6c236d1c54fea77178ea7a9bd5adaf3d3f5
Alejandro Fernandez | U-Payments <alejandro.fernandez@u-payments.com>
2020-07-15 08:23:42 -0400

    feat inicializar proyecto

---
+++ src/test/java/com/ozymern/spring/microservices/config/server/SpringMicroservicesConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozymern.spring.microservices.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroservicesConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
